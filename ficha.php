568824121258eec5c0203d867a799f5cad13fce4
julio <jberzal86@gmail.com>
2017-04-06 13:29:06 +0200

    Comentarios y formateado para aclarar codigo

--- ficha.php
+++ ficha.php
@@ -11,7 +11,14 @@ $m = new Mustache_Engine(array(
 
 //var_dump($values);
 foreach ($values as $key=>$row) {
-    $arr = array('nombre'=>$row[0],'imagen'=>$row[1],'descripcion'=>nl2br($row[2]),'ano'=>$row[3], 'id'=>$key);
+
+  	// Modificar aquí según los campos necesarios de la hoja de cáculo. Los nombres se asocian después a la plantilla Mustache
+    $arr = array(
+    	'nombre'=>$row[0],
+    	'imagen'=>$row[1],
+    	'descripcion'=>nl2br($row[2]),
+    	'ano'=>$row[3], 
+    	'id'=>$key);
 }
 //$template = $m->loadTemplate('ficha');
 echo $m->render('ficha', $arr);
--- lista.php
+++ lista.php
@@ -10,7 +10,13 @@ $m = new Mustache_Engine(array(
 $arr = array();
 foreach ($values as $key=>$row) {
   if ($key>0 && $row[0] != '')
-    $arr['grupos'][] = array('nombre'=>$row[0],'imagen'=>$row[1],'descripcion'=>nl2br($row[2]),'ano'=>$row[3], 'id'=>$key);
+
+  	// Modificar aquí según los campos necesarios de la hoja de cáculo. Los nombres se asocian después a la plantilla Mustache
+    $arr['grupos'][] = array(
+    	'nombre'=>$row[0],
+    	'imagen'=>$row[1],
+    	'ano'=>$row[3], 
+    	'id'=>$key);
 }
 //$template = $m->loadTemplate('list');
 echo $m->render('list', $arr);
